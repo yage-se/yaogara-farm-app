c412462390a70df566c8af6e04ef700d42b84161
DaddyGringo <40545026+gringodaddy@users.noreply.github.com>
2025-06-04 18:50:22 -0500

    Updated fetch URL to new deployment

--- js/forms.js
+++ js/forms.js
@@ -6,13 +6,12 @@ document.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
 
     const formData = new FormData(form);
-    const data = Object.fromEntries(formData);
+    formData.append("module", "huevos");
 
     try {
       const response = await fetch("https://script.google.com/macros/s/AKfycbxAHiSDMFFOMBRfpVRYI5ruPKbqeJOq_EudZPv81N1JH7BTWyXsAbQ48XLgi0Au56J1jQ/exec", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ module: "huevos", ...data }),
+        body: formData,
       });
 
       const result = await response.json();
